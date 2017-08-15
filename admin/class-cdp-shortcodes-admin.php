01849e2015d25e4a06ecb770ed0289226b969d12
Scott Gustas <scottius@gmail.com>
2017-08-15 15:38:44 -0400

    Rework plugin using CMB2

--- admin/class-cdp-shortcodes-admin.php
+++ admin/class-cdp-shortcodes-admin.php
@@ -1,63 +1,112 @@
-<?php
- 
+<?php 
+
 class CDP_Shortcodes_Admin {
  
     protected $version;
  
     public function __construct( $version ) {
         $this->version = $version;
-        $this->define_cdp_shortcodes();
-        $this->load_cdp_shortcodes();
-    }
- 
-    public function enqueue_styles() {
-        //wp_enqueue_style( 'cdp-shortcodes-admin', plugin_dir_url( __FILE__ ) . 'css/cdp-shortcodes-admin.css', array(), $this->version, FALSE );
+        $this->shortcode = 'cdp_shortcode';
+        $this->modulesSrc = 'http://iip-design-stage-modules.s3-website-us-east-1.amazonaws.com/modules/';
+        $this->load_cdp_shortcode();
     }
 
-    public function enqueue_scripts() {
-        //wp_enqueue_script('cdp-shortcodes-admin', plugin_dir_url(__FILE__) . 'js/cdp-shortcodes-admin.js');
+    public function init_my_shortcode_button() {
+
+      // Set up the button data that will be passed to the javascript files
+      $js_button_data = array(
+        'qt_button_text' => __( 'CDP Shortcodes', 'cdp-shortcodes' ),
+        'button_tooltip' => __( 'CDP Shortcodes', 'cdp-shortcodes' ),
+        // Temp icon. Replace with 20x20 CDP icon
+        'icon'           => 'dashicons-networking',
+
+        // Use your own textdomain
+        'l10ncancel'     => __( 'Cancel', 'cdp-shortcodes' ),
+        'l10ninsert'     => __( 'Insert Shortcode', 'cdp-shortcodes' )
+      );
+
+      // Optional additional parameters
+      $additional_args = array(
+        // Can be a callback or metabox config array
+        'cmb_metabox_config'   => array($this, 'shortcode_button_cmb_config'),
+        // Set the conditions of the shortcode buttons
+        'conditional_callback' => array($this, 'shortcode_button_only_admin')
+      );
+
+      $button = new Shortcode_Button( $this->shortcode, $js_button_data, $additional_args );
+
+    }
+
+    public function change_assets_url(){
+      $file = dirname(__FILE__) . '/../vendor/jtsternberg/Shortcode_Button/shortcode-button.php';
+      $plugin_url = plugin_dir_url($file);
+      return $plugin_url;
+    }
+
+    /**
+     * Return CMB2 config array
+     *
+     * @param  array  $button_data Array of button data
+     *
+     * @return array               CMB2 config array
+     */
+    public function shortcode_button_cmb_config( $button_data ) {
+
+      return array(
+        'id'     => 'cdp_shortcodes_'. $button_data['slug'],
+        'fields' => array(
+          array(
+            'name'             => 'Choose CDP Shortcode',
+            'desc'             => 'Select a CDP shortcode to insert.',
+            'id'               => 'type',
+            'type'             => 'select',
+            'show_option_none' => false,
+            'default'          => 'article-feed',
+            'options'          => array(
+              'article-feed' => __( 'Article Feed', 'cdp-shortcodes' ),
+              //'course'   => __( 'Course', 'cdp-shortcodes' ),
+              //'video'     => __( 'Video', 'cdp-shortcodes' ),
+            ),
+          ),
+          array(
+            'name'    => 'Sites',
+            'desc'    => 'Choose which sites you would like to display',
+            'id'      => 'sites',
+            'type'    => 'multicheck',
+            'options' => array(
+              'share.america.gov' => 'ShareAmerica',                
+              'yali.state.gov' => 'YALI',
+              'ylai.state.gov' => 'YLAI'
+            ),
+          ),
+        ),
+        // keep this w/ a key of 'options-page' and use the button slug as the value
+        'show_on' => array( 'key' => 'options-page', 'value' => $button_data['slug'] ),
+      );
+
+    }
+
+    /**
+     * Callback dictates that shortcode button will only display if we're on a 'page' edit screen
+     *
+     * @return bool Expects a boolean value
+     */
+    public function shortcode_button_only_admin() {
+      if ( ! is_admin() ) {
+        return false;
+      }
+
+      // Ok, guess we're on an admin edit screen
+      return true;
     }
 
     public function define_cdp_shortcodes() {
         $this->shortcodesArray = array('Course', 'Article_Feed');
         //Todo: This URL should be stored elsewhere.
-        $this->modulesSrc = 'http://iip-design-stage-modules.s3-website-us-east-1.amazonaws.com/modules/';
     }
 
-    public function load_cdp_shortcodes() {
-        foreach($this->shortcodesArray as $shortcode) {
-            $shortcode = 'cdp-' . strtolower(str_replace( '_', '-', $shortcode));
-            add_shortcode($shortcode, array($this, 'cdp_shortcodes_output'));
-        }
-    }
-    
-    public function load_cdp_styles_scripts() {
-        //Todo: This should be moved outside admin.
-        global $post;
-        foreach($this->shortcodesArray as $shortcode) {
-            $shortcode = 'cdp-' . strtolower(str_replace( '_', '-', $shortcode));
-            $src = $this->modulesSrc . $shortcode;
-            wp_register_style($shortcode, $src . '.min.css');
-            wp_register_script($shortcode, $src . '.min.js');
-            if( is_a( $post, 'WP_Post' ) && has_shortcode( $post->post_content, $shortcode) ) {
-                wp_enqueue_script($shortcode);
-                wp_enqueue_style($shortcode);
-            }
-        } 
-    }
-
-    public function cdp_shortcodes_get_shortcodes () {
-        if( !empty($this->shortcodesArray) ) {
-            
-            echo '<script type="text/javascript">
-            var cdp_menu = new Object();';
-            foreach( $this->shortcodesArray as $item )  {
-                $sc = 'cdp_' . strtolower($item);
-                $item = str_replace( '_', ' ', $item );
-                echo "cdp_menu['{$sc}'] = '{$item}';";
-            }
-            echo '</script>';
-        }
+    public function load_cdp_shortcode() {
+      add_shortcode($this->shortcode, array($this, 'cdp_shortcodes_output'));
     }
 
     public function cdp_shortcodes_output($atts, $content = "", $shortcode) {
@@ -70,23 +119,27 @@ class CDP_Shortcodes_Admin {
             mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
         );
 
-        $shortcode = "<div id='$uuid' rel='$shortcode' ";
+        $shortcode = "<div id='$uuid' ";
+
         foreach($atts as $key => $val){
+          if ( $key === 'type' ) {
+            $type = $val;
+            $shortcode .= "rel=$type ";
+          } else {
             $shortcode .= "data-$key=$val ";
+          }
         }
+
         $shortcode .= "></div>";
+
+        $moduleURL = $this->modulesSrc . 'cdp-module-' . $type . '/' . $type . '.min.js';
+        $moduleCSS = $this->modulesSrc . 'cdp-module-' . $type . '/' . $type . '.min.css';
+
+        $shortcode .= "<script type='text/javascript' src=$moduleURL></script>";
+        $shortcode .= "<link rel='stylesheet' id=$type href=$moduleCSS type='text/css' media='all'></style>";
 
         return $shortcode;
     }
-
-    public function cdp_shortcodes_add_buttons( $plugin_array ) {
-        $plugin_array['cdp_shortcodes'] = plugin_dir_url( __FILE__ )  . 'js/src/tinymce/cdp.tinymce.plugins.min.js';
-        return $plugin_array;
-    }
-    
-    public function cdp_shortcodes_register_buttons( $buttons ) {
-        array_push($buttons, 'cdp_shortcode');
-        return $buttons;
-    } 
- 
 }
+
+?>
--- includes/class-cdp-shortcodes.php
+++ includes/class-cdp-shortcodes.php
@@ -18,6 +18,8 @@ class CDP_Shortcodes {
     }
 
     private function load_dependencies() {
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'vendor/webdevstudios/cmb2/init.php';
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'vendor/jtsternberg/Shortcode_Button/shortcode-button.php';
         require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-cdp-shortcodes-admin.php';
         require_once plugin_dir_path( __FILE__ ) . 'class-cdp-shortcodes-loader.php';
         $this->loader = new CDP_Shortcodes_Loader();
@@ -28,14 +30,16 @@ class CDP_Shortcodes {
         $admin = new CDP_Shortcodes_Admin( $this->get_version() );
 
         // load actions
-        $this->loader->add_action( 'admin_enqueue_scripts', $admin, 'enqueue_scripts' );
-        $this->loader->add_action( 'admin_enqueue_scripts', $admin, 'enqueue_styles' );
-        $this->loader->add_action( 'wp_enqueue_scripts', $admin, 'load_cdp_styles_scripts');
-        $this->loader->add_action( 'admin_footer', $admin, 'cdp_shortcodes_get_shortcodes' );
+        //$this->loader->add_action( 'admin_enqueue_scripts', $admin, 'enqueue_scripts' );
+        //$this->loader->add_action( 'admin_enqueue_scripts', $admin, 'enqueue_styles' );
+        //$this->loader->add_action( 'wp_enqueue_scripts', $admin, 'load_cdp_styles_scripts');
+        //$this->loader->add_action( 'admin_footer', $admin, 'cdp_shortcodes_get_shortcodes' );
+        $this->loader->add_action( 'shortcode_button_load', $admin, 'init_my_shortcode_button', ( SHORTCODE_BUTTONS_LOADED + 1 ) );
 
         // load filters
-        $this->loader->add_filter( 'mce_external_plugins', $admin, 'cdp_shortcodes_add_buttons' );
-        $this->loader->add_filter( 'mce_buttons', $admin, 'cdp_shortcodes_register_buttons' );
+        //$this->loader->add_filter( 'mce_external_plugins', $admin, 'cdp_shortcodes_add_buttons' );
+        //$this->loader->add_filter( 'mce_buttons', $admin, 'cdp_shortcodes_register_buttons' );
+        $this->loader->add_filter( 'shortcode_button_assets_url', $admin, 'change_assets_url' );
     
     }
  
